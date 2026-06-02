b37078c185f00a30a98fc399227bd81170f2051c
Dmitriy <lanskoyydi17@gmail.com>
2026-06-02 23:47:06 +0300

    Fix service_test.go: use mocks instead of real FileProducer/FilePresenter

--- masker/service_test.go
+++ masker/service_test.go
@@ -1,36 +1,82 @@
 package masker
 
 import (
-	"os"
-	"testing"
+    "context"
+    "errors"
+    "log/slog"
+    "os"
+    "testing"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 )
 
-func TestNewFilePresenter(t *testing.T) {
-	p := NewFilePresenter("out.txt")
-	assert.NotNil(t, p)
-	assert.Equal(t, "out.txt", p.path)
+type mockProducer struct {
+    data []string
+    err  error
 }
 
-func TestFilePresenterPresentSuccess(t *testing.T) {
-	tmpFile := createTempFile(t, []string{})
-	deleteTempFile(tmpFile)
-
-	p := NewFilePresenter(tmpFile)
-	data := []string{"hello", "world"}
-	err := p.Present(data)
-	assert.NoError(t, err)
-
-	result, err := readFileLines(tmpFile)
-	assert.NoError(t, err)
-	assert.Equal(t, data, result)
-
-	os.Remove(tmpFile)
+func (m *mockProducer) Produce(ctx context.Context) ([]string, error) {
+    return m.data, m.err
 }
 
-func TestFilePresenterPresentCreateError(t *testing.T) {
-	p := NewFilePresenter("/nonexistent_dir_12345/out.txt")
-	err := p.Present([]string{"test"})
-	assert.Error(t, err)
+type mockPresenter struct {
+    receivedData []string
+    err          error
+}
+
+func (m *mockPresenter) Present(ctx context.Context, data []string) error {
+    m.receivedData = data
+    return m.err
+}
+
+type mockMasker struct {
+    result string
+}
+
+func (m *mockMasker) Mask(ctx context.Context, line string) string {
+    return m.result
+}
+
+func TestServiceRunSuccess(t *testing.T) {
+    logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+    prod := &mockProducer{data: []string{"a1", "b2"}, err: nil}
+    pres := &mockPresenter{err: nil}
+    mask := &mockMasker{result: "***"}
+    svc := NewService(prod, pres, mask, logger)
+
+    err := svc.Run(context.Background())
+
+    assert.NoError(t, err)
+    assert.Equal(t, []string{"***", "***"}, pres.receivedData)
+}
+
+func TestServiceRunProducerError(t *testing.T) {
+    logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+    prod := &mockProducer{data: nil, err: errors.New("produce error")}
+    pres := &mockPresenter{}
+    mask := &mockMasker{result: "***"}
+    svc := NewService(prod, pres, mask, logger)
+
+    err := svc.Run(context.Background())
+
+    assert.Error(t, err)
+    assert.Nil(t, pres.receivedData)
+}
+
+func TestServiceRunContextCancel(t *testing.T) {
+    logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+    ctx, cancel := context.WithCancel(context.Background())
+    cancel()
+
+    prod := &mockProducer{data: []string{"a1", "b2"}, err: nil}
+    pres := &mockPresenter{err: nil}
+    mask := &mockMasker{result: "***"}
+    svc := NewService(prod, pres, mask, logger)
+
+    err := svc.Run(ctx)
+
+    assert.Error(t, err)
 }
